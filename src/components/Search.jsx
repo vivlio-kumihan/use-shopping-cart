878761c6ef92962465ac1ec4076495c602ad0ffe
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-03-14 11:49:11 +0900

    read cord

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,21 @@
 //components/Search.js
 import React from 'react';
- 
-function Search({ searchCourse, courseSearchUserFunction }) {
+
+const Search = ({ searchCourse, courseSearchUserFn }) => {
   return (
     <header className="App-header">
       <h1>GeeksforGeeks Shopping Cart</h1>
       <div className="search-bar">
+        {/* 課題　インプットに入力しても文字が表示されない問題。 */}
         <input
           type="text"
           placeholder="Search for GFG Products..."
           value={searchCourse}
-          onChange={courseSearchUserFunction}
+          onChange={courseSearchUserFn}
         />
       </div>
     </header>
   );
-}
- 
+};
+
 export default Search;
--- src/components/ShowCourse.jsx
+++ src/components/ShowCourse.jsx
@@ -1,34 +1,38 @@
 //components/ShowCourse.js^$\n
 import React from 'react';
 
+import "../sass/ShowCourse.sass";
+
 const ShowCourse = ({
   courses, 
-  filterCourseFunction, 
-  addCourseToCartFunction,
+  addCourseToCartFn,
+  filterCourseFn, 
 }) => {
   return (
     <div className="product-list">
-      {filterCourseFunction.length === 0 ? (
-        <p className="no-results">
-          Sorry Geek, No matching Product found.
-        </p>
-      ) : (
-        filterCourseFunction.map((product) => (
-          <div className="product" key={product.id}>
-            <img src={product.image} alt={product.name} />
-            <h2>{product.name}</h2>
-            <p>Price: ₹{product.price}</p>
-            <button
-              className="add-to-cart-button"
-              onClick={() => addCourseToCartFunction(product)}
-            >
-              Add to Shopping Cart
-            </button>
-          </div>
-        ))
-      )}
+      {filterCourseFn.length === 0 
+        ? (
+      <p className="no-results">
+        Sorry Geek, No matching Product found.
+      </p>
+        ) 
+        : (
+      filterCourseFn.map((product) => (
+      <div className="product" key={product.id}>
+        <img src={product.image} alt={product.name} />
+        <h2>{product.name}</h2>
+        <p>Price: ₹{product.price}</p>
+        <button
+          className="add-to-cart-button"
+          onClick={() => addCourseToCartFn(product)}
+        >
+          Add to Shopping Cart
+        </button>
+      </div>))
+        )
+      }
     </div>
-  );  
+  );
 };
 
 export default ShowCourse;
